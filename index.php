60375063417053957784c08350e8f6084fca6810
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-26 15:22:39 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+
+Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche.
+
+L'e-commerce vende prodotti per gli animali.
+I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+
+L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
+
+Il pagamento avviene con la carta di credito, che non deve essere scaduta.
+BONUS: Alcuni prodotti (es. antipulci) avranno la caratteristica che saranno disponibili solo in un periodo particolare (es. da maggio ad agosto).
+
+Tips:
+Qualora aveste bisogno di passare ad un metodo un tipo di dato particolare, come ad esempio un oggetto che avete creato voi, potete usarlo come facciamo di solito al posto di Type $variable.
+Infondo anche le nostre classi sono dei tipi di dato.
+
+Ad esempio
+public function functionName( MyClass $variable, String $variable2, Int $variable3 ) {
+# Implementazione
+} 
+
+
+*/
+
+
+
