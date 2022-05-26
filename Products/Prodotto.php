a910a2046a61164d7c3d925ed088429acf881152
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-26 18:41:04 +0200

    add new objects in Prodotto class

--- Products/Prodotto.php
+++ Products/Prodotto.php
@@ -5,15 +5,17 @@
 class Prodotto {
 
     public $nome;
-    public $categoria;
+    public $categoria_prodotto;
+    public $categoria_animale;
     public $descrizione;
     public $prezzo;
     public $disponibilita;
 
-    function __construct(String $nome, String $categoria, String $descrizione, mixed $prezzo, int $disponibilita)
+    function __construct(String $nome, String $categoria_prodotto, String $categoria_animale, String $descrizione, mixed $prezzo, int $disponibilita)
     {
         $this -> nome = $nome;
-        $this -> categoria = $categoria;
+        $this -> categoria_prodotto = $categoria_prodotto;
+        $this -> categoria_animale = $categoria_animale;
         $this -> descrizione = $descrizione;
         $this -> prezzo = $prezzo;
         $this -> disponibilita = $disponibilita;
--- index.php
+++ index.php
@@ -46,5 +46,20 @@ $utenteRegistrato -> setSconto(true);
 var_dump($utenteRegistrato);
 
 
+/* creo oggetti per la classe Prodotto  */
+
+$crocchette = new Prodotto ('friskies','cibo','cane', 'Le crocchette per il tuo cane con Manzo, Cereali e verdure',23.43, 50 );
+
+$cuccia = new Prodotto ('Letto Basic', 'oggetto', 'gatto', 'Letto per gatti in morbida finta pelliccia rivestita esternamente con resistente tessuto.', 15.99, 3 );
+
+$pallina = new Prodotto ('Palla- Matix', 'giochi', 'gatto', 'Simpaticissima pallina in gomma per il tuo migliore amico a quattro zampe', 5, 20 );
+
+$corda = new Prodotto ('Trixie - Corda Osso', 'giochi', 'cane', 'Trixie ha studiato una linea di giochi ideali non solo a far divertire il tuo cane, ma anche utilissimi per aiutare il tuo pet a mantenere la sua igiene orale. Con la coloratissima corda per cani di Trixie in misto cotone, la cura dell’igiene orale del tuo cane sarà molto più divertente del previsto!', 4.13, 0);
+
+$prodotti=[];
+array_push($prodotti, $crocchette, $cuccia, $pallina, $corda);
+var_dump($prodotti);
+
+
 
 
